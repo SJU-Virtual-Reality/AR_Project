7171f2ac67da8dcb4c3da8ebd2dab4d22e3fad00
Yohan <ztf1205@naver.com>
2023-11-04 14:37:43 +0900

    큐브 클릭 기능 추가
    -AR 입력 테스트를 위해서 예제 씬에서 큐브 클릭하면 비활성화하는 기능 추가

---
+++ AR Project/Assets/Test/Yohan/TestARObjectController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARKit;
+
+public class TestARObjectController : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit) && hit.transform.gameObject.tag == "Player")
+                {
+                    hit.transform.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
